61f2c7a355c1e116666f73a462dffeab304c4ff4
Ameen <mohammedameen028@gmail.com>
2021-05-23 13:03:43 +0530

    adding the comments to server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//API_KEY and PORT from .env file
 const PORT = process.env.PORT || 3005;
 const API_KEY = process.env.API_KEY
 
@@ -12,6 +13,8 @@ const HOST = 'https://newsapi.org/v2'
 const app = express();
 app.use(cors()) 
 
+
+//API to get the latest news
 app.get("/getLatestNews",(req, res) => {
     axios.get(`${HOST}/top-headlines?country=gb&apiKey=${API_KEY}`)
     .then((response) => {
@@ -19,6 +22,7 @@ app.get("/getLatestNews",(req, res) => {
     })
   });
 
+//API to search based on keyword
 app.get("/search", (req, res) => {
     const { keyword } = req.query
     axios.get(`${HOST}/everything?q=${keyword}&apiKey=${API_KEY}`)
